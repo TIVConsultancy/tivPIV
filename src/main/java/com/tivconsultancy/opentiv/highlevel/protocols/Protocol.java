0ce82385848007f9c27b2ac3f02be4aeafb6e9f9
TIVConsultancy <62260362+TIVConsultancy@users.noreply.github.com>
2020-05-20 15:29:19 -0700

    Merge origin/master
    Conflicts:
    	.gitignore

--- src/main/java/com/tivconsultancy/opentiv/highlevel/protocols/Protocol.java
+++ src/main/java/com/tivconsultancy/opentiv/highlevel/protocols/Protocol.java
@@ -5,10 +5,13 @@
  */
 package com.tivconsultancy.opentiv.highlevel.protocols;
 
+import com.tivconsultancy.opentiv.helpfunctions.settings.Hints;
 import com.tivconsultancy.opentiv.helpfunctions.settings.SettingObject;
 import com.tivconsultancy.opentiv.helpfunctions.settings.Settings;
+import com.tivconsultancy.opentiv.helpfunctions.settings.SettingsCluster;
 import com.tivconsultancy.opentiv.imageproc.primitives.ImageInt;
 import com.tivconsultancy.opentiv.math.specials.LookUp;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,7 +19,7 @@ import java.util.List;
  * @author Thomas Ziegenhein
  * @param <T>
  */
-public abstract class Protocol extends Settings {
+public abstract class Protocol extends Settings implements Hints {
     
     protected LookUp<ImageInt> outPutImages;
     
@@ -34,6 +37,21 @@ public abstract class Protocol extends Settings {
     public abstract Double getOverTimesResult(NameSpaceProtocolResults1D ident);
     public abstract void run(Object... input) throws UnableToRunException;
     public abstract Object[] getResults();
+    
+    public List<SettingObject> getHints(){
+        List<SettingObject> ls = new ArrayList<>();
+        for (SettingsCluster c : getClusters()) {
+                for (SettingObject o : c.getSettings()) {
+                    if (o.ident.equals(SettingObject.SettingsType.Boolean)) {
+                        ls.add(o);
+                        ls.add(new SettingObject(o.getName(), !((Boolean) o.sValue), SettingObject.SettingsType.Boolean));
+                    } else {
+                        ls.add(o);
+                    }
+                }
+            }
+        return ls;
+    }
     
     
 }
