ccd6feaf7cf610d862f4c0da3562dd9a3d5c2cc4
TIVConsultancy <62260362+TIVConsultancy@users.noreply.github.com>
2020-05-28 16:43:41 -0700

    update

--- src/main/java/com/tivconsultancy/tivpiv/PIVMethod.java
+++ src/main/java/com/tivconsultancy/tivpiv/PIVMethod.java
@@ -20,7 +20,6 @@ import com.tivconsultancy.tivpiv.protocols.Prot_PIVInterrAreas;
 import com.tivconsultancy.tivpiv.protocols.Prot_PIVObjectMasking;
 import com.tivconsultancy.tivpiv.protocols.Prot_PIVPreProcessor;
 import com.tivconsultancy.tivpiv.protocols.Prot_PIVRead2IMGFiles;
-import com.tivconsultancy.tivpiv.protocols.Prot_PIVRead2IMGFiles2;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
